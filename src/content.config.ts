53522c19bff1d59581e91fffff37560d02c5a05d
Hasan Naim <finova.front@gmail.com>
2025-01-23 13:58:39 +0200

    new

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,21 +1,21 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-// Post collection schema
-const blogCollection = defineCollection({
-  loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/blog" }),
-  schema: z.object({
-    title: z.string(),
-    meta_title: z.string().optional(),
-    description: z.string().optional(),
-    date: z.date().optional(),
-    image: z.string().optional(),
-    author: z.string().default("Admin"),
-    categories: z.array(z.string()).default(["others"]),
-    tags: z.array(z.string()).default(["others"]),
-    draft: z.boolean().optional(),
-  }),
-});
+// // Post collection schema
+// const blogCollection = defineCollection({
+//   loader: glob({ pattern: "**/*.{md,mdx}", base: "src/content/blog" }),
+//   schema: z.object({
+//     title: z.string(),
+//     meta_title: z.string().optional(),
+//     description: z.string().optional(),
+//     date: z.date().optional(),
+//     image: z.string().optional(),
+//     author: z.string().default("Admin"),
+//     categories: z.array(z.string()).default(["others"]),
+//     tags: z.array(z.string()).default(["others"]),
+//     draft: z.boolean().optional(),
+//   }),
+// });
 
 // Author collection schema
 const authorsCollection = defineCollection({
@@ -151,7 +151,7 @@ const testimonialSectionCollection = defineCollection({
 export const collections = {
   // Pages
   homepage: homepageCollection,
-  blog: blogCollection,
+  // blog: blogCollection,
   authors: authorsCollection,
   pages: pagesCollection,
   about: aboutCollection,
